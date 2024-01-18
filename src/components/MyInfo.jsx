57adbfd80f0da987f314daf3154ff5bd5ac3a1f9
Kaung Sett Hein <43985555+KaungSettHein@users.noreply.github.com>
2024-01-18 05:43:48 +0000

    created new components and folder

---
+++ src/components/MyInfo.jsx
@@ -0,0 +1,17 @@
+import "./MyInfo.css";
+
+function MyInfo() {
+    return(
+        <div className="MyInfo">
+            <h1>Kaung Sett Hein</h1>
+            <p>This is my first react application</p>
+            <ul>
+                <li>Hawaii</li>
+                <li>New Zealand</li>
+                <li>Switzerland</li>
+            </ul>
+        </div>
+    )
+}
+
+export default MyInfo;
