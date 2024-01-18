11665fe007c27b9e50d7fbe130582df1c614c9cb
Kaung Sett Hein <43985555+KaungSettHein@users.noreply.github.com>
2024-01-18 08:58:32 +0000

    use css and js

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,27 @@ import MyInfo from './components/MyInfo';
 import Headers from './components/Header';
 
 function App() {
+  const date = new Date();
+  const hours = date.getHours();
+  let dayTime
+  const styles = {
+    fontSize : 38
+  }
+
+  if(hours < 12){
+    dayTime = "morning";
+  }else if(hours >=12 && hours < 17){
+    dayTime = "afternoon";
+  }else{
+    dayTime = "night";
+  }
+
   return (
     <div className='App'>
       <Headers />
-      <MyInfo />
-      <Footer/>
+      <h1 style={styles}>Good {dayTime}!</h1>
+      {/* <MyInfo /> */}
+      {/* <Footer/> */}
     </div>
   );
 }
