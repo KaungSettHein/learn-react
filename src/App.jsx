1e80cb786d2cfaabb03da10401e221b0c30d5139
Kaung Sett Hein <43985555+KaungSettHein@users.noreply.github.com>
2024-01-19 00:40:32 +0000

    first react lesson

--- src/App.jsx
+++ src/App.jsx
@@ -6,25 +6,28 @@ import Headers from './components/Header';
 function App() {
   const date = new Date();
   const hours = date.getHours();
-  let dayTime
+  let dayTime;
   const styles = {
     fontSize : 38
   }
 
-  if(hours < 12){
-    dayTime = "morning";
-  }else if(hours >=12 && hours < 17){
-    dayTime = "afternoon";
-  }else{
-    dayTime = "night";
-  }
+  // if(hours < 12){
+  //   dayTime = "morning";
+  //   styles.color = yellow;
+  // }else if(hours >=12 && hours < 17){
+  //   dayTime = "afternoon";
+  //   styles.color = red;
+  // }else{
+  //   dayTime = "night";
+  //   styles.color = orange;
+  // }
 
   return (
     <div className='App'>
       <Headers />
       <h1 style={styles}>Good {dayTime}!</h1>
-      {/* <MyInfo /> */}
-      {/* <Footer/> */}
+      <MyInfo />
+      <Footer/>
     </div>
   );
 }
